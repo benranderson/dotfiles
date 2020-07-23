400078617dc5c37bef275b36f75fcb6724a6eb78
benranderson <ben.m.randerson@gmail.com>
2020-07-23 09:36:28 +0100

    reinstall pipx packages if homebrew python is upgraded

--- dotfiles/update-tools.sh
+++ dotfiles/update-tools.sh
@@ -2,9 +2,13 @@ echo "Updating tools..."
 echo "Updating Homebrew..."
 brew update
 echo "Updating Homebrew packages..."
-brew upgrade
-echo "Updating pipx..."
-pipx upgrade-all
+if brew upgrade | grep -q 'Upgrading python'; then
+    echo "Reinstalling pipx packages due to Python upgrade..."
+    pipx reinstall-all --verbose
+else
+    echo "Updating pipx..."
+    pipx upgrade-all --verbose
+fi
 echo "Updating pyenv..."
 pyenv update
 echo "Tools updated!"
