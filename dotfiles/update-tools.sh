931f5149f7d6344333e8167cbc946e5d8a7e7d44
benranderson <ben.m.randerson@gmail.com>
2020-07-31 09:55:40 +0100

    add brew cleanup to update script and remove verbose flag on pipx commands

--- dotfiles/update-tools.sh
+++ dotfiles/update-tools.sh
@@ -4,11 +4,13 @@ brew update
 echo "Updating Homebrew packages..."
 if brew upgrade | grep -q 'Upgrading python'; then
     echo "Reinstalling pipx packages due to Python upgrade..."
-    pipx reinstall-all --verbose
+    pipx reinstall-all
 else
     echo "Updating pipx..."
-    pipx upgrade-all --verbose
+    pipx upgrade-all
 fi
+echo "Cleaning up Homebrew..."
+brew cleanup
 echo "Updating pyenv..."
 pyenv update
 echo "Tools updated!"
