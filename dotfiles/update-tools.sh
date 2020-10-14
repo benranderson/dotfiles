c6df3cd738f0d3e8204c7d4a31d5cadad2d12506
benranderson <ben.m.randerson@gmail.com>
2020-10-14 22:17:55 +0100

    refine update tools script

--- dotfiles/update-tools.sh
+++ dotfiles/update-tools.sh
@@ -2,13 +2,17 @@ echo "Updating tools..."
 echo "Updating Homebrew..."
 brew update
 echo "Updating Homebrew packages..."
-if brew upgrade | grep -q 'Upgrading python'; then
+OUTPUT=$(brew upgrade)
+echo ${OUTPUT}
+if [[ $OUTPUT == *"Upgrading python."* ]]; then
     echo "Reinstalling pipx packages due to Python upgrade..."
     pipx reinstall-all
 else
     echo "Updating pipx..."
     pipx upgrade-all
 fi
+echo "Updating brew casks..."
+brew upgrade --cask
 echo "Cleaning up Homebrew..."
 brew cleanup
-echo "Tools updated!"
+echo "Tools updated! 🎉"
