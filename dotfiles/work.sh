ee7201ab8baa8d28583601edf6eb7a2cb63f21ea
benranderson <ben.randerson@technipfmc.com>
2021-05-05 10:46:13 +0100

    add obsidian to work script

--- dotfiles/work.sh
+++ dotfiles/work.sh
@@ -9,6 +9,8 @@ echo "Opening Microsoft Teams..."
 open -a Microsoft\ Teams
 echo "Opening Tasks spreadsheet..."
 open /Users/benranderson/OneDrive\ -\ Digicorner/Documents/Management/Tasks.xlsx
+echo "Opening Obsidian..."
+open -a Obsidian
 echo "Opening FastPipe in VS Code..."
 code /Users/benranderson/Documents/dev/fastpipe
 echo "Workspace set up!"
