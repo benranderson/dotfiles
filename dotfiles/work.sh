7e62107d885bcaa35759b28a48c473f12adf77f2
benranderson <ben.randerson@technipfmc.com>
2024-03-11 13:59:45 +0000

    update teams app name and add cargo to zshenv

--- dotfiles/work.sh
+++ dotfiles/work.sh
@@ -2,11 +2,11 @@ echo "Setting up workspace..."
 echo "Opening Azure Boards"
 open -a Safari https://dev.azure.com/technipfmc-dev/SubseaDesign/_boards/board/t/Developers
 echo "Opening Microsoft Outlook..."
-open -a Microsoft\ Outlook
+open -a "Microsoft Outlook"
 echo "Opening Microsoft Teams..."
-open -a Microsoft\ Teams\ classic
+open -a "Microsoft Teams (work or school)"
 echo "Opening Tasks spreadsheet..."
-open /Users/benranderson/OneDrive\ -\ Digicorner/Management/Tasks.xlsx
+open "/Users/benranderson/OneDrive - Digicorner/Management/Tasks.xlsx"
 echo "Opening Obsidian..."
 open -a Obsidian
 echo "Opening digital workspace in VS Code..."
